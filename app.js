983330bb35f9225c70e52694ed6f6c9dafbb1b94
sximenez <70152164+sximenez@users.noreply.github.com>
2022-09-20 23:30:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Declare variables
+
+let celsius = document.getElementById("cel").value;
+let fahrenheit = document.getElementById("far").value;
+let btnConvert = document.getElementById("convert").value;
+
+// Declare action
