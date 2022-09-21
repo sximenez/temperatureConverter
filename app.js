0835503bb9655b6d2864dd41c4a43ed930a608b1
sximenez <70152164+sximenez@users.noreply.github.com>
2022-09-21 09:44:20 +0200

    update

--- app.js
+++
@@ -1,7 +0,0 @@
-// Declare variables
-
-let celsius = document.getElementById("cel").value;
-let fahrenheit = document.getElementById("far").value;
-let btnConvert = document.getElementById("convert").value;
-
-// Declare action
